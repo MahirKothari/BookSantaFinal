5af7c97fe8a1213b640c8b9c7a0c27d26f7cff87
MahirKothari <mahirdk07@gmail.com>
2021-03-26 16:52:03 +0530

    'finalBookSanta'

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+  
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './screens/WelcomeScreen.js'
-import {AppTabNavigator} from './components/AppTabNavigator'
-import {createAppContainer,createSwitchNavigator} from 'react-navigation'
-import {AppDrawerNavigator} from './components/AppDrawerNavigator'
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+import { AppTabNavigator } from './components/AppTabNavigator'
+
 
 export default function App() {
   return (
-    
-    <AppContainer>
-
-    </AppContainer>
+    <AppContainer/>
   );
 }
+
+
 const switchNavigator = createSwitchNavigator({
-  WelcomeScreen:{screen:WelcomeScreen},
-  Drawer:{screen:AppDrawerNavigator}
+  WelcomeScreen:{screen: WelcomeScreen},
+  Drawer:{screen: AppDrawerNavigator},
+  BottomTab: {screen: AppTabNavigator},
 })
-const AppContainer =  createAppContainer(switchNavigator)
+
+const AppContainer =  createAppContainer(switchNavigator);
--- components/AppTabNavigator.js
+++ components/AppTabNavigator.js
@@ -1,26 +1,23 @@
-import React,{Component}from 'react'; 
-import {Image} from 'react-native';
-import {createBottomTabNavigator} from 'react-navigation-tabs'
-import BookDonateScreen from '../screens/BookDonateScreen'
-import BookRequestScreen from '../screens/BookRequestScreen'
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { AppStackNavigator } from './AppStackNavigator'
+import BookRequestScreen from '../screens/BookRequestScreen';
+
 
 export const AppTabNavigator = createBottomTabNavigator({
-    DonateBooks:{
-        screen:BookDonateScreen,
-        navigationOptions:{
-            tabBarIcon:<Image source = {require('../assets/book1.png')} style = {{width:20,height:20}}>
-
-                       </Image>,
-                       tabBarLabel:"DonateBooks"
-        }
-    },
-    BookRequest:{
-        screen:BookRequestScreen,
-        navigationOptions:{
-            tabBarIcon:<Image source = {require('../assets/book2.png')} style = {{width:20,height:20}}>
-
-            </Image>,
-            tabBarLabel:"BookRequest"
-        }
+  DonateBooks : {
+    screen: AppStackNavigator,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/request-list.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Donate Books",
     }
-})
+  },
+  BookRequest: {
+    screen: BookRequestScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/request-book.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Book Request",
+    }
+  }
+});
